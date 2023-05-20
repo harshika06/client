67e42138f77575fe55e722c03c607c420aa2e828
Harshika <harshikasaxena2206@gmail.com>
2023-05-20 18:33:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import {useRef,useState,useEffect} from 'react';
 import './App.css';
+import  {uploadFile} from './services/api'
 
 function App() {
+  const [file,setFile] = useState('');
+  const [result,setResult] = useState('');
+  
+  const fileInputRef = useRef();
+
+   
+  const logo = 'https://cutewallpaper.org/22/phone-technology-wallpapers/297164689.jpg'
+   const onUploadClick =() =>
+   {
+     fileInputRef.current.click();
+   }
+
+   useEffect(()=>
+   {
+    const getImage = async() =>
+    {
+      if(file)
+      {
+        const data = new FormData();
+        data.append('name',file.name);
+        data.append('file',file);
+
+
+
+       let response = await uploadFile(data);
+       setResult(response.path);
+
+
+
+      }
+
+    }
+    getImage();
+   },[file])
+
+  
+  console.log(file)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className = "container" >
+         <img src= {logo} alt = "banner"></img>
+        <div className="wrapper">
+          <h1>File Sharing Application </h1>
+          <p>Upload and Share the download link</p>
+          <button  onClick={()=> onUploadClick()} >Upload</button>
+            <input type="file"
+            ref = {fileInputRef}
+            style={{display:'none'}}
+            onChange={(e)=>setFile(e.target.files[0])}
+           
+           /> 
+           <a href={result} target="_blank">{result}</a>        </div>
+      </div>
   );
 }
 
